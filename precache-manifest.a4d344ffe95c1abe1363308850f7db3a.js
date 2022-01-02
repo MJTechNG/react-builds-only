e00d363496331cfe0b488e12fb6271b29dc597e2
Jamilu Salisu <jamilusalis@gmail.com>
2022-01-02 07:38:59 +0100

    init

---
+++ precache-manifest.a4d344ffe95c1abe1363308850f7db3a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "1eb834fbed7342056b9b",
+    "url": "/static/css/main.6e40410a.chunk.css"
+  },
+  {
+    "revision": "1eb834fbed7342056b9b",
+    "url": "/static/js/main.e9c9c802.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "45a329279dd907375c62",
+    "url": "/static/css/2.052cc549.chunk.css"
+  },
+  {
+    "revision": "45a329279dd907375c62",
+    "url": "/static/js/2.7818ca11.chunk.js"
+  },
+  {
+    "revision": "bad19771db42d9c212abd5da4fe7a827",
+    "url": "/static/media/login.bad19771.svg"
+  },
+  {
+    "revision": "52ecff6cbe03ca614889389eea209698",
+    "url": "/static/media/auth.52ecff6c.svg"
+  },
+  {
+    "revision": "0ae84278c0acb287545837100dc8b419",
+    "url": "/static/media/update.0ae84278.svg"
+  },
+  {
+    "revision": "48a94696ccd8563fb115a9f5bf45b840",
+    "url": "/static/media/welcome.48a94696.svg"
+  },
+  {
+    "revision": "8b8a8dc67dd958c0f2f8de5fd9c662f0",
+    "url": "/static/media/forget.8b8a8dc6.svg"
+  },
+  {
+    "revision": "8f55d1cb11a98b6c3ae9328137def4a0",
+    "url": "/static/media/reset.8f55d1cb.svg"
+  },
+  {
+    "revision": "c2a68da36b76c36176b13b97fc8f1e5a",
+    "url": "/index.html"
+  }
+];
